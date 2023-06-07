366215a13fdebb1c01b831b835153d72105e1332
Laweens Francois <lfrancois@brooklynsteamcenter.org>
2023-06-07 14:53:32 -0400

    almost done

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import {createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from './views/HomePage.vue'
+import search from './views/search.vue'
 
 const routes = [
     {
         path: '/', 
         component: HomePage 
+    },
+    {
+        path: '/', 
+        component: search
     },
 ]
 
---
+++ src/views/search.vue
@@ -0,0 +1,42 @@
+<script>
+
+export default {
+    data() {
+        return {
+            query:'',
+            results: []
+        };
+    },
+    methods: {
+        search() {
+            fetch("https://freesound.org/apiv2/search/text/?query=piano&token=U08a0ydB5JLZ6alofwKXbn48IPYWXjY8H53IneRy")
+            .then(response => response.json())
+            .then(data => {
+                this.results = data.results;
+            })
+        .catch(error => {
+            console.error(error);
+        }) ;
+    },
+   asyncFunction() {
+    fetch("https://freesound.org/apiv2/search/text/?query=piano&token=U08a0ydB5JLZ6alofwKXbn48IPYWXjY8H53IneRy")
+    .then(response => {
+        console.log(response);
+    });
+   },
+ }
+}
+</script>
+<template>
+<v-list-item>
+    <v-list-item-title>
+        <v-text-field v-model="searchText" label="Search" @Keydown.enter="search" />
+         </v-list-item-title>
+         </v-list-item>
+<v-list-item v-for="result in restults" :key="result.id" link>
+    <v-list-item-title>{{ results.name }}</v-list-item-title>
+</v-list-item>
+</template>
+
+<style scoped>
+</style>
